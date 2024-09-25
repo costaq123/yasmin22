8d13f6839150518ab9c69a6a845696304126ab62
costaq123 <142261370+costaq123@users.noreply.github.com>
2024-09-25 11:28:20 -0300

    p

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
       alert ('nicole e yasmin')
-      function soma (numero1, numero2) {
+      function soma (numero1, numero2){
         return + numero1+numero2;
     }
     console.log (soma (5,2));
