48dbb1e296d561ab5354924f3fc38edaa302acdc
costaq123 <142261370+costaq123@users.noreply.github.com>
2024-09-25 12:22:11 -0300

    t

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
-      alert ('nicole e yasmin')
-      function soma (numero1, numero2){
-        return + numero1+numero2;
+   alert('nicole e yasmin')
+function soma(numero1, numero2) {
+  return + numero1 + numero2;
+}
+console.log(soma(5, 2));
+function calcularAreaQuadrado(lado) {
+  return lado * lado;
+}
+console.log(calcularAreaQuadrado(6));
+let numero = prompt("qual sua idade?")
+let media = fuction(x, y, z){
+  return x+y + z / 3;
     }
-    console.log (soma (5,2));
-    function calcularAreaQuadrado(lado){
-      return lado*lado;
-    }
-    console.log (calcularAreaQuadrado (6));
-    let numero=prompt ("qual sua idade?")
-     document.write (prompt ('?'))
-    let media=fuction (x,y,z){
-    return x+y+z/3 ;
-    }
-    console.log (media (2,5,6))
+console.log(media(2, 5, 6))
