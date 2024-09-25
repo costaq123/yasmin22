b68beb38de3e1c437c6f6c5cec59cf39fafa0e67
costaq123 <142261370+costaq123@users.noreply.github.com>
2024-09-25 12:27:46 -0300

    f

--- main.js
+++ main.js
@@ -7,9 +7,9 @@ function calcularAreaQuadrado(lado) {
   return lado * lado;
 }
 console.log(calcularAreaQuadrado(6));
-let numero = prompt("qual sua idade?");
+let numero = prompt("qual é a sua idade?");
 document.write('qual é a sua idade?')
-let media = function(x, y, z){
+let media= function (x, y, z){
   return x+y+z / 3;
     }
 console.log(media(2, 5, 6))
