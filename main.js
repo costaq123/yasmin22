45e5a31bf4f32a398444bf674bec025913691971
costaq123 <142261370+costaq123@users.noreply.github.com>
2024-10-02 11:36:43 -0300

    A

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-   alert('nicole e yasmin')
-function soma(numero1, numero2) {
-  return + numero1 + numero2;
-}
-console.log(soma(5, 2));
-function calcularAreaQuadrado(lado) {
-  return lado * lado;
-}
-console.log(calcularAreaQuadrado(6));
-let numero = prompt("qual é a sua idade?");
-document.write('qual é a sua idade?')
-let media= function (x, y, z){
-  return x+y+z / 3;
-    }
-console.log(media(2, 5, 6))
+//    alert('nicole e yasmin')
+// function soma(numero1, numero2) {
+//   return + numero1 + numero2;
+// }
+// console.log(soma(5, 2));
+// function calcularAreaQuadrado(lado) {
+//   return lado * lado;
+// }
+// console.log(calcularAreaQuadrado(6));
+// let numero = prompt("qual é a sua idade?");
+// document.write('qual é a sua idade?')
+// let media= function (x, y, z){
+//   return x+y+z / 3;
+//     }
+// console.log(media(2, 5, 6))
+
+
+let estados=['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',]
