4555b550aa4544a86e10f695b7cb0debcaa19911
costaq123 <142261370+costaq123@users.noreply.github.com>
2024-10-02 12:25:48 -0300

    o

--- main.js
+++ main.js
@@ -15,15 +15,23 @@
 // console.log(media(2, 5, 6))
 
 
-let estados=['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',]
-let i=0;
-while( i<estados.length) {
-console.log (i);
-console.log(estados[i]);
-i=i+1;
-}
-for (let index = 0; index < estados.length; index++) {
-    console.log (index);
-    console.log(estados[index]);
+// let estados=['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',]
+// let i=0;
+// while( i<estados.length) {
+// console.log (i);
+// console.log(estados[i]);
+// i=i+1;
+// }
+// for (let index = 0; index < estados.length; index++) {
+//     console.log (index);
+//     console.log(estados[index]);
     
+// }
+
+let idade= 16;
+if(idade>17){
+console.log('pode dirigir');
+}
+else{
+    console.log ('ilegal dirigir');
 }
