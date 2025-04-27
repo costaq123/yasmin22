e744f0311e5b7349f12b43e4326d4d3930112c2e
costaq123 <142261370+costaq123@users.noreply.github.com>
2025-04-27 17:00:39 -0300

    j

--- main.js
+++ main.js
@@ -1,37 +1,29 @@
-//    alert('nicole e yasmin')
-// function soma(numero1, numero2) {
-//   return + numero1 + numero2;
-// }
-// console.log(soma(5, 2));
-// function calcularAreaQuadrado(lado) {
-//   return lado * lado;
-// }
-// console.log(calcularAreaQuadrado(6));
-// let numero = prompt("qual é a sua idade?");
-// document.write('qual é a sua idade?')
-// let media= function (x, y, z){
-//   return x+y+z / 3;
-//     }
-// console.log(media(2, 5, 6))
-
-
-// let estados=['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO',]
-// let i=0;
-// while( i<estados.length) {
-// console.log (i);
-// console.log(estados[i]);
-// i=i+1;
-// }
-// for (let index = 0; index < estados.length; index++) {
-//     console.log (index);
-//     console.log(estados[index]);
-    
-// }
-
-let idade= 16;
-if(idade>17){
-console.log('pode dirigir');
-}
-else{
-    console.log ('ilegal dirigir');
-}
+document.addEventListener('DOMContentLoaded', function() {
+    const botaoDeAcessibilidade = document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade = document.getElementById('opcoes-acessibilidade');
+  
+    botaoDeAcessibilidade.addEventListener('click', function () {
+      botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+      opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+  
+      const botaoSelecionado = botaoDeAcessibilidade.getAttribute('aria-expanded') === 'true';
+      botaoDeAcessibilidade.setAttribute('aria-expanded', !botaoSelecionado);
+    });
+  
+    // Funcionalidade de aumento e diminuição de fonte
+    const aumentaFonteBotao = document.getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document.getElementById('diminuir-fonte');
+  
+    let tamanhoAtualFonte = 1;
+  
+    aumentaFonteBotao.addEventListener('click', function() {
+      tamanhoAtualFonte += 0.1;
+      document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+  
+    diminuiFonteBotao.addEventListener('click', function() {
+      tamanhoAtualFonte -= 0.1;
+      document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+  });
+  
